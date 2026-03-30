5c46170c8d603cde67f74c95f1dd359566f9e086
Akshay-Kumar-SSR <akshaykumar27.1.2007@gmail.com>
2026-03-30 23:01:31 +0530

    final portfolio

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
-console.log("Portfolio Loaded 🚀");
+AOS.init();
+
+// Scroll
+function scrollToSection() {
+  document.getElementById("about").scrollIntoView({
+    behavior: "smooth"
+  });
+}
+
+// Typing
+const text = "Founder • Developer • Visionary";
+let i = 0;
+
+function typeEffect() {
+  if (i < text.length) {
+    document.getElementById("typing").innerHTML += text.charAt(i);
+    i++;
+    setTimeout(typeEffect, 50);
+  }
+}
+typeEffect();
+
+// Cursor
+const cursor = document.querySelector(".cursor");
+
+document.addEventListener("mousemove", (e) => {
+  cursor.style.top = e.clientY + "px";
+  cursor.style.left = e.clientX + "px";
+});
